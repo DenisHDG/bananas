ece5cf2f7c218da35eb005f5f58b1edcc1427209
Denis Gonçalves <denis.goncalves@southsystem.com.br>
2023-05-22 17:55:12 -0300

    Initial commit

---
+++ src/main/java/com/example/abacaxi/Carambola.kt
@@ -0,0 +1,12 @@
+package com.example.abacaxi
+
+import android.content.Context
+import android.widget.Toast
+
+class Carambola {
+
+    fun joio(context: Context){
+        Toast.makeText(context, "Olá Amige", Toast.LENGTH_LONG).show()
+    }
+
+}
